5c7c4fe5325e8c8d41a5c4ba22fd3f6ef966cdf2
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-06-09 12:02:38 -0600

    Challenge 1 complete

--- react_tech_interview/src/App.js
+++ react_tech_interview/src/App.js
@@ -19,69 +19,130 @@ const Card = styled.div`
   outline: 5px solid purple;
 `
 
-function App() {
-
-  const exampleTask = {
-    id: uuid(),
-    title: 'Name of Task',
-    description: 'Things I need to complete'
+class App extends React.Component {
+  state = {
+    title: '',
+    description: '',
+    tasks: [{id: uuid(), title: "shya", description: "right"}],
+    inProgress: [],
+    done: []
   }
 
-  const newTask = (e) => {
+  newTask = (e) => {
     e.preventDefault();
-    console.log('Create Task')
-    // Create a new task here
+    this.addReadyTasks(uuid(), this.state.title, this.state.description)
   }
 
-  const showTask = (item) => {
+   showTask = (item) => {
     return (
     <Card key={item.id}>
         <h3>{item.title}</h3>
         <p>{item.description}</p>
         <br></br>
-        <button>{'<'}</button>
-        <button> {'>'}</button>
+        <button onClick={(e) => this.statusRevert(item, e)}>{'<'}</button>
+        <button onClick={(e) => this.statusChange(item, e)}> {'>'}</button>
         <br></br>
-        <button onClick={() => deleteTask()}>Delete</button>
+        <button onClick={(e) => this.deleteTask(item, e)}>Delete</button>
       </Card>
     )
   }
 
-  const deleteTask = () => {
-    console.log('Delete Task')
-    // Delete a task here
+  addReadyTasks = (id, title, description) => {
+    this.setState({
+      tasks: [...this.state.tasks, {id: id, title: title, description: description}] 
+    })
   }
 
+  deleteTask = (item) => {
+    if (this.state.tasks.includes(item)) {
+      this.setState({
+        tasks: this.state.tasks.filter((task) => task.id !== item.id)
+      })
+    } else if (this.state.inProgress.includes(item)) {
+      this.setState({
+        inProgress: this.state.inProgress.filter((task) => task.id !== item.id)
+      })
+    } else {
+      this.setState({
+        done: this.state.done.filter((task) => task.id !== item.id)
+      })
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  statusChange = (item) => {
+    if (this.state.tasks.includes(item)) {
+    this.setState({
+      tasks: this.state.tasks.filter((task) => task.id !== item.id),
+      inProgress: [...this.state.inProgress, item]
+    })} else {
+      this.setState({
+        inProgress: this.state.inProgress.filter((task) => task.id !== item.id),
+        done: [...this.state.done, item]
+      })
+    }
+  }
+
+  statusRevert = (item) => {
+    if (this.state.done.includes(item)) {
+    this.setState({
+      done: this.state.done.filter((task) => task.id !== item.id),
+      inProgress: [...this.state.inProgress, item]
+    })} else {
+      this.setState({
+        inProgress: this.state.inProgress.filter((task) => task.id !== item.id),
+        tasks: [...this.state.tasks, item]
+      })
+    }
+  }
+  
+  render() {
   return (
     <Container>
       <Column>
       <h1>KANBAN BOARD</h1>
-        <form>
-        <label for="titlen">Title:</label>
+        <form onSubmit={this.newTask}>
+        <label for="title">Title:</label>
         <br></br>
-        <input type="text" id="title" name="title" />
+        <input type="text" id="title" name="title" value={this.state.title} onChange={this.handleChange}/>
         <br></br>
         <label for="description">Description:</label>
         <br></br>
-        <input type="text" id="description" name="description" />
+        <input type="text" id="description" name="description" value={this.state.description} onChange={this.handleChange}/>
         <br></br>
-        <input type="submit" value="Submit" onClick={(e) => newTask(e)}/>
+        <input type="submit" value="Submit" />
         </form>
       </Column>
       <Column>
-        <h2>Todo</h2>
-        {showTask(exampleTask)}
+        <h2>Ready</h2>
+        <div>
+        {this.state.tasks.map((task) => {
+          return this.showTask(task)
+        })}
+        </div>
       </Column>
       <Column>
         <h2>In Progress</h2>
-        <p>Put items here</p>
+        <div>
+        {this.state.inProgress.map((task) => {
+          return this.showTask(task)
+        })}
+        </div>
       </Column>
       <Column>
       <h2>Done</h2>
-        <p>Put items here</p>
+        {this.state.done.map((task) => {
+          return this.showTask(task)
+        })}
       </Column>
     </Container>
   );
+  }
 }
 
 export default App
